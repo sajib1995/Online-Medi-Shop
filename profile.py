9997c1ae60bfc9ccba0199df3df3e244f3994122
atiqursohan <87199259+atiqursohan@users.noreply.github.com>
2021-09-06 10:05:47 +0600

    Profile all functions

---
+++ profile.py
@@ -0,0 +1,31 @@
+from django.views.generic import View, TemplateView, CreateView, FormView, DetailView, ListView
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.core.paginator import Paginator
+from .utils import password_reset_token
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from django.conf import settings
+from django.db.models import Q
+from .models import *
+from .forms import *
+import requests
+
+class CustomerProfileView(TemplateView):
+    template_name = "customerprofile.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and Customer.objects.filter(user=request.user).exists():
+            pass
+        else:
+            return redirect("/login/?next=/profile/")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        customer = self.request.user.customer
+        context['customer'] = customer
+        orders = Order.objects.filter(cart__customer=customer).order_by("-id")
+        context["orders"] = orders
+        return context
