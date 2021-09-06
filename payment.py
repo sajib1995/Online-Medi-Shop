0d483e91b51ed9318257e5e76fe93d4f1caac3e4
atiqursohan <87199259+atiqursohan@users.noreply.github.com>
2021-09-06 09:26:04 +0600

    Payment Method all functions

---
+++ payment.py
@@ -0,0 +1,93 @@
+from django.views.generic import View, TemplateView, CreateView, FormView, DetailView, ListView
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.core.paginator import Paginator
+from .utils import password_reset_token
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from django.conf import settings
+from django.db.models import Q
+from .models import *
+from .forms import *
+import requests
+
+class KhaltiRequestView(View):
+    def get(self, request, *args, **kwargs):
+        o_id = request.GET.get("o_id")
+        order = Order.objects.get(id=o_id)
+        context = {
+            "order": order
+        }
+        return render(request, "khaltirequest.html", context)
+
+
+class KhaltiVerifyView(View):
+    def get(self, request, *args, **kwargs):
+        token = request.GET.get("token")
+        amount = request.GET.get("amount")
+        o_id = request.GET.get("order_id")
+        print(token, amount, o_id)
+
+        url = "https://khalti.com/api/v2/payment/verify/"
+        payload = {
+            "token": token,
+            "amount": amount
+        }
+        headers = {
+            "Authorization": "Key test_secret_key_f59e8b7d18b4499ca40f68195a846e9b"
+        }
+
+        order_obj = Order.objects.get(id=o_id)
+
+        response = requests.post(url, payload, headers=headers)
+        resp_dict = response.json()
+        if resp_dict.get("idx"):
+            success = True
+            order_obj.payment_completed = True
+            order_obj.save()
+        else:
+            success = False
+        data = {
+            "success": success
+        }
+        return JsonResponse(data)
+
+
+class EsewaRequestView(View):
+    def get(self, request, *args, **kwargs):
+        o_id = request.GET.get("o_id")
+        order = Order.objects.get(id=o_id)
+        context = {
+            "order": order
+        }
+        return render(request, "esewarequest.html", context)
+
+
+class EsewaVerifyView(View):
+    def get(self, request, *args, **kwargs):
+        import xml.etree.ElementTree as ET
+        oid = request.GET.get("oid")
+        amt = request.GET.get("amt")
+        refId = request.GET.get("refId")
+
+        url = "https://uat.esewa.com.np/epay/transrec"
+        d = {
+            'amt': amt,
+            'scd': 'epay_payment',
+            'rid': refId,
+            'pid': oid,
+        }
+        resp = requests.post(url, d)
+        root = ET.fromstring(resp.content)
+        status = root[0].text.strip()
+
+        order_id = oid.split("_")[1]
+        order_obj = Order.objects.get(id=order_id)
+        if status == "Success":
+            order_obj.payment_completed = True
+            order_obj.save()
+            return redirect("/")
+        else:
+
+            return redirect("/esewa-request/?o_id="+order_id)
